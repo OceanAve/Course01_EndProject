e0bd04167e79c9ef6657cc8009784c107467d100
J. Uriarte <juriarte@oceanavestudios.com>
2024-08-24 14:40:41 -0700

    Added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ClientsComponent } from "./clients/clients.component";
+import { ClientMeetingsComponent } from "./client-meetings/client-meetings.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ClientsComponent, ClientMeetingsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
