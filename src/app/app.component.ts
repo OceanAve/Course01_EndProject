31e2210cfc77942334940fb0ae042b02da4bd5be
J. Uriarte <juriarte@oceanavestudios.com>
2024-08-27 12:38:44 -0700

    chore: Update dependencies and add login component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,31 +2,34 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ClientsComponent } from "./clients/clients.component";
 import { ClientMeetingsComponent } from "./client-meetings/client-meetings.component";
+import { ProjectsComponent } from "./projects/projects.component";
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from "./login/login.component";
+import { SideBarComponent } from "./side-bar/side-bar.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule, ClientsComponent, ClientMeetingsComponent],
+  imports: [RouterOutlet, FormsModule, ClientsComponent, ClientMeetingsComponent, ProjectsComponent, LoginComponent, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'CMA';
-  name: string = "Igor";
-  isDisabled: boolean = true;
-  message: string = ""
+//   name: string = "Igor";
+//   isDisabled: boolean = true;
+//   message: string = ""
 
-  saveMessage: string = "";
-  onSave(): void {
-    this.saveMessage = "Message Saved";
-  console.log("Saved");
-}
+//   saveMessage: string = "";
+//   onSave(): void {
+//     this.saveMessage = "Message Saved";
+//   console.log("Saved");
+// }
 
-  constructor() {
-    setTimeout(() => {
-      this.isDisabled = false;
-    }, 3000);
-  }
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.isDisabled = false;
+  //   }, 3000);
+  // }
 }
