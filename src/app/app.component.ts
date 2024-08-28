2869abd8a3a3b4391aa0b6d681c366fb6f81485b
J. Uriarte <juriarte@oceanavestudios.com>
2024-08-28 09:58:02 -0700

    added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+// import { RouterOutlet} from '@angular/router';
 import { ClientsComponent } from "./clients/clients.component";
 import { ClientMeetingsComponent } from "./client-meetings/client-meetings.component";
 import { ProjectsComponent } from "./projects/projects.component";
-import { FormsModule } from '@angular/forms';
 import { LoginComponent } from "./login/login.component";
 import { SideBarComponent } from "./side-bar/side-bar.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, ClientsComponent, ClientMeetingsComponent, ProjectsComponent, LoginComponent, SideBarComponent],
+  imports: [ FormsModule, ClientsComponent, ClientMeetingsComponent, ProjectsComponent, LoginComponent, SideBarComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'CMA';
-//   name: string = "Igor";
-//   isDisabled: boolean = true;
-//   message: string = ""
 
-//   saveMessage: string = "";
-//   onSave(): void {
-//     this.saveMessage = "Message Saved";
-//   console.log("Saved");
-// }
-
-  // constructor() {
-  //   setTimeout(() => {
-  //     this.isDisabled = false;
-  //   }, 3000);
-  // }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { Routes} from '@angular/router';
+import { ClientsComponent } from './clients/clients.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { LoginComponent } from './login/login.component';
+import { ClientMeetingsComponent } from './client-meetings/client-meetings.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: ProjectsComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'clients', component: ClientsComponent },
+    { path: 'client-meetings', component: ClientMeetingsComponent },
+    // { path: 'dashboard', component: DashboardComponent },
+    { path: '**', redirectTo: 'login', pathMatch: 'full' }
+];
